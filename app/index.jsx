8dcc9c98cde62e3c4d2f0ac9c8698937ab1ddda5
André Abreu de Sá <andre.abreu.ufmg@gmail.com>
2023-07-01 16:29:37 +0000

    Initial React Navite ExpoRouter

---
+++ app/index.jsx
@@ -0,0 +1,10 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+export default function app() {
+  return (
+    <View className=' flex-1 items-center justify-center bg-red-800' >
+      <Text className={`text-2xl text-yellow-400`} >Gitpod NativeWind Expo Router Tunnel Funcionando !</Text>
+    </View>
+  )
+}
